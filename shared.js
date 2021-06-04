be4471abf809f9f6bd63313df8c3731912785fb0
Kedar Patil <knpatil512@gmail.com>
2021-06-04 20:49:16 +0530

    Added transitions and animations

--- shared.js
+++ shared.js
@@ -8,6 +8,7 @@ let lastName = document.querySelector('#last-name');
 let email = document.querySelector('#email');
 let password = document.querySelector('#password');
 let submitButton = document.querySelector('#submit-button');
+let ctaButton = document.querySelector('.main-nav__item--cta');
 
 const validationObj = {
     'first-name': {
@@ -130,7 +131,10 @@ const showModal = (e) => {
     // modal.style.display = 'block';
     // backdrop.style.display = 'block';
     modal.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = "block";
+    setTimeout(() => {
+        backdrop.classList.add('open');
+    }, 10);
 }
 
 const hideModal = (e) => {
@@ -141,6 +145,9 @@ const hideModal = (e) => {
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+    setTimeout(() => {
+        backdrop.style.display = "none";
+    }, 500); // Time should be same as transition duration. check shared.css
 
     if (null !== mobileNav) {
         mobileNav.classList.remove('open');
@@ -148,11 +155,26 @@ const hideModal = (e) => {
 }
 
 toggleButton.addEventListener('click', () => {
-    // mobileNav.style.display = 'block';
+    //mobileNav.style.display = 'block';
+    backdrop.style.display = "block";
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    setTimeout(() => {
+        backdrop.classList.add('open');
+    }, 10);
 });
 
 for (button of choosePlanButtons) {
     button.addEventListener('click', showModal);
 }
+
+ctaButton.addEventListener('animationstart', event => {
+    console.log('Animation started', event);
+});
+
+ctaButton.addEventListener('animationiteration', event => {
+    console.log('Animation iteration', event);
+});
+
+ctaButton.addEventListener('animationend', event => {
+    console.log('Animation ended', event);
+});
